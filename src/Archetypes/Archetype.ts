59107357d65d8bf61ae4d8b37d21f1d22cb72140
Thayara Siqueira <thay.siqu@gmail.com>
2022-09-25 13:23:13 -0300

    feat: adds abstract class Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string, special = 0, cost = 0) {
+    this._name = name;
+    this._special = special;
+    this._cost = cost;
+  }
+
+  public get name(): string { return this._name; }
+
+  public get special(): number { return this._special; }
+  
+  public get cost(): number { return this._cost; }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energytype(): EnergyType;
+}
